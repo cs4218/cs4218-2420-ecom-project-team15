dde0bd957d72c847e679775854f5da16d414bb40
sohjehou <e0694467@u.nus.edu>
2025-02-12 12:15:07 +0800

    Fix merge conflict

--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,3 @@
 import { TextDecoder, TextEncoder } from 'util';
 global.TextEncoder = TextEncoder;
-<<<<<<< HEAD
 global.TextDecoder = TextDecoder;
-=======
-global.TextDecoder = TextDecoder;
->>>>>>> f5d1d1da8b6cc298c3add77edc3b2e7bb7637ca4
