dafdfcf28eb61ed27911ad20b0107678ae15b975
GlennOngJunJie <glennongjj@gmail.com>
2025-03-09 21:13:56 +0800

    Remove debugging statement

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -136,8 +136,6 @@ const getAllProducts = async () => {
     setChecked(all);
   };
   useEffect(() => {
-    console.log("HELLLOOO")
-    console.log(!checked.length || !radio.length)
     if (!checked.length || !radio.length) getAllProducts();
   }, [checked.length, radio.length]);
 
