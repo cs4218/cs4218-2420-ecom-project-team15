f0976d6022a6174a5969315249c73f5a74539d0c
sohjehou <e0694467@u.nus.edu>
2025-03-12 22:30:43 +0800

    Add forgot password page

---
+++ client/src/pages/Auth/ForgotPassword.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import Layout from "./../../components/Layout";
+import axios from "axios";
+import { useNavigate, useLocation } from "react-router-dom";
+import toast from "react-hot-toast";
+import "../../styles/AuthStyles.css";
+
+const ForgotPassword = () => {
+  const [email, setEmail] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  // form function
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/api/v1/auth/forgot-password", {
+        email,
+        answer,
+        newPassword: password,
+      });
+      if (res && res.data.success) {
+        toast.success(res.data && res.data.message, {
+            duration: 5000,
+            icon: "🙏",
+            style: {
+              background: "green",
+              color: "white",
+            },
+          });
+        navigate("/login");
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+  return (
+    <Layout title="Forgot Password - Ecommerce App">
+      <div className="form-container " style={{ minHeight: "90vh" }}>
+        <form onSubmit={handleSubmit} style={{ display: "flex", alignItems: "center", flexDirection: "column" }}>
+          <h4 className="title">FORGOT PASSWORD FORM</h4>
+
+          <div className="mb-3" style={{ width: "100%" }}>
+            <input
+              type="email"
+              autoFocus
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter Your Email "
+              required
+            />
+          </div>
+          <div className="mb-3" style={{ width: "100%" }}>
+            <input
+              type="text"
+              value={answer}
+              onChange={(e) => setAnswer(e.target.value)}
+              className="form-control"
+              id="exampleInputAnswer1"
+              placeholder="Enter Your Answer"
+              required
+            />
+          </div>
+          <div className="mb-3" style={{ width: "100%" }}>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Enter Your New Password"
+              required
+            />
+          </div>
+          <button type="submit" className="btn btn-primaryr" style={{ width: "100%" }}>
+            Forgot Password
+          </button>
+        </form>
+      </div>
+    </Layout>
+  );
+};
+
+export default ForgotPassword;
